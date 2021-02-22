686261364b972807a2d2e5607473be52f7c6c81b
matevosyanmher <66181653+matevosyanmher@users.noreply.github.com>
2021-02-23 00:30:41 +0400

    OrdArray

---
+++ src/OrdArray.java
@@ -0,0 +1,94 @@
+public class OrdArray {
+    private long a[];
+    private int nElems;
+
+    public OrdArray(int max) {
+        a = new long[max];
+        nElems = 0;
+    }
+
+    public int size() {
+        return nElems;
+    }
+
+    public int find(long serchKey) {
+        int lowerBound = 0;
+        int upperBound = nElems - 1;
+        int curIn;
+
+        while (true) {
+            curIn = (upperBound - lowerBound) / 2;
+            if (curIn == serchKey) {
+                return curIn;
+            } else if (lowerBound > upperBound) {
+                return nElems;
+            } else {
+                if (a[curIn] < serchKey) {
+                    lowerBound = curIn - 1;
+                } else upperBound = curIn + 1;
+            }
+
+        }
+    }
+
+    public void insert(long value) {
+        int j;
+        for (j = 0; j < nElems; j++) {
+            if (a[j] > value)
+                break;
+
+            for (int k = nElems; k > j; k--)
+                a[k] = a[k - 1];
+                a[j] = value;
+            nElems++;
+        }
+    }
+
+    public boolean delete(long value) {
+        int j = find(value);
+        if (j == nElems) // Найти не удалось
+            return false;
+        else // Элемент найден
+        {
+            for (int k = j; k < nElems; k++) // Перемещение последующих элементов
+                a[k] = a[k + 1];
+            nElems--; // Уменьшение размера
+            return true;
+        }
+    }
+
+    public void display() // Вывод содержимого массива
+    {
+        for (int j = 0; j < nElems; j++) // Перебор всех элементов
+            System.out.print(a[j] + " "); // Вывод текущего элемента
+        System.out.println("");
+    }
+}
+
+class OrderedApp {
+    public static void main(String[] args) {
+        int maxSize = 100; // Размер массива
+        OrdArray arr; // Ссылка на массив
+        arr = new OrdArray(maxSize); // Создание массива
+        arr.insert(77);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        int searchKey = 55; // Поиск элемента
+        if (arr.find(searchKey) != arr.size())
+            System.out.println("Found " + searchKey);
+        else
+            System.out.println("Can't find " + searchKey);
+        arr.display(); // Вывод содержимого
+        arr.delete(00); // Удаление трех элементов
+        arr.delete(55);
+        arr.delete(99);
+        arr.display(); // Повторный вывод
+    }
+} //Конец класса OrderedApp
